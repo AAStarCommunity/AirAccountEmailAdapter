ff56aabe25765cb67ec516d929d2a4a8f995da73
徐超越 <993921@qq.com>
2024-02-26 23:18:13 +0800

    email instructions

--- email/parser.go
+++ email/parser.go
@@ -2,12 +2,25 @@ package email
 
 import (
 	"AirAccountEmailAdapter/pkg"
+	"strings"
 	"time"
 )
 
+type PolicyType int
+
 // OpParser get instructions from *op string
 func OpParser(msg *string) *pkg.Op {
+
+	var action pkg.OpActionType
+	if strings.EqualFold(*msg, string(pkg.BindWallet)) {
+		action = pkg.BindWallet
+	} else if strings.EqualFold(*msg, string(pkg.QueryBalance)) {
+		action = pkg.QueryBalance
+	} else {
+		// TODO: the rest action
+	}
 	return &pkg.Op{
+		Action:    action,
 		Timestamp: time.Time{},
 		From:      "stub",
 		To:        "stub",
