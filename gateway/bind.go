ccc14c7b6793d224519d4200fed03a29fa11f8a8
徐超越 <993921@qq.com>
2024-03-19 20:04:16 +0800

    email instructions

--- gateway/bind.go
+++ gateway/bind.go
@@ -1,7 +1,25 @@
 package gateway
 
-import "AirAccountEmailAdapter/pkg"
+import (
+	"AirAccountEmailAdapter/conf"
+	"AirAccountEmailAdapter/infra"
+	"AirAccountEmailAdapter/pkg"
+	"bytes"
+	"net/http"
+)
 
-func bind(m *pkg.Op) {
-
+func bind(m *pkg.Op) error {
+	cfg := conf.Get().AAGateway.Host
+	if resp, err := http.Post(cfg+"/api/instructions/bind?id="+m.From, "application/json", bytes.NewBuffer([]byte("{}"))); err != nil {
+		return err
+	} else {
+		if resp.StatusCode == http.StatusOK {
+			go infra.ReplyEmail(m.From, "Congratulations! Your AirAccount Created!", m.Message)
+		} else if resp.StatusCode == http.StatusNotAcceptable {
+			go infra.ReplyEmail(m.From, "Your AirAccount Already Exists!", m.Message)
+		} else {
+			return err
+		}
+	}
+	return nil
 }
